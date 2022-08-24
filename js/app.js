f8c50f137eb2e554570ce379cfa393dd1d23c10a
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-08-24 11:36:37 +0200

    Added my design mockup as a png. Split event listener up to have a seperate listener for number buttons and function buttons to support functional calculator logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 window.onload = function() {
 
   const numButtons = document.getElementsByClassName('numButtons')
+  const funButtons = document.getElementsByClassName('funButtons')
 
   Array.from(numButtons).forEach( (button) => {
-    button.addEventListener('click', () => 
-      {console.log(button.innerText)})
+    button.addEventListener('click', () => {
+      console.log(button.innerText)
+    })
+  })
+
+  Array.from(funButtons).forEach ((button) => {
+    button.addEventListener('click', () => {
+      {console.log(button.innerText)}
+    })
   })
 }

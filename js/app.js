740802a14438124993ab061f37913ebe02000468
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-08-24 15:41:09 +0200

    Added new listener for equals sign and running empty calculation function when clicked. Calculation function needs to be coded still

--- js/app.js
+++ js/app.js
@@ -1,35 +1,65 @@
 window.onload = function() {
 
-  let calcSteps = document.getElementById('calcSteps')
-  let calcResults = document.getElementById('calcSolution')
+  const showCalcSteps = document.getElementById('calcSteps')
+  const showCalcResults = document.getElementById('calcResults')
+  const equals = document.getElementById('equals')
 
   const numButtons = document.getElementsByClassName('numButtons')
   const funButtons = document.getElementsByClassName('funButtons')
 
+  let didCalculate = false
+
+  const calculate = () => {
+    console.log('calculate')
+  }
+
   Array.from(numButtons).forEach( (button) => {
     button.addEventListener('click', () => {
-      let steps = calcSteps.innerText
+      let steps = showCalcSteps.innerText
       let checkForDecimal = steps.split(/[\+\-\÷\×]/).pop()
+
+      if (didCalculate) {
+        steps = ''
+        didCalculate = false
+      }
+
+      console.log(didCalculate)
+      
       if (isNaN(steps[steps.length - 1 ]) && 
         !((steps[steps.length - 1 ]) === '.')) {
-          calcSteps.innerText = steps + ' ' + button.innerText
+          showCalcSteps.innerText = steps + ' ' + button.innerText
       } else if ((button.innerText === '.') && checkForDecimal.indexOf('.') === -1) {
-        calcSteps.innerText = steps + button.innerText
+        showCalcSteps.innerText = steps + button.innerText
       } else if ((button.innerText !== '.')) {
-        calcSteps.innerText = steps + button.innerText
+        showCalcSteps.innerText = steps + button.innerText
       }
     })
   })
 
   Array.from(funButtons).forEach ((button) => {
     button.addEventListener('click', () => {
-      let steps = calcSteps.innerText
+      let steps = showCalcSteps.innerText
+
+      if (didCalculate) {
+        showCalcSteps.innerText = 'Add calculation results back up here'
+        didCalculate = false
+      }
+
       if (isNaN(steps[steps.length - 1 ])) {
         return
       } else {
         buttonText = button.innerText
-        calcSteps.innerText = calcSteps.innerText + ' ' + buttonText + ' '
+        showCalcSteps.innerText = showCalcSteps.innerText + ' ' + buttonText + ' '
       }
     })
+  })
+
+  equals.addEventListener('click', () => {
+    showCalcSteps.innerText = showCalcSteps.innerText + ' ' + '='
+
+    calculate() 
+    showCalcResults.innerText = 'Add calculation'
+
+    didCalculate = true
   })
 }

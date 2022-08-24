f944a943c71451b1c4c7f33c880e98b73ecbc514
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-08-24 11:19:27 +0200

    Added event listener to all num buttons. The inner test of each button is being logging on click.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 window.onload = function() {
+
+  const numButtons = document.getElementsByClassName('numButtons')
+
+  Array.from(numButtons).forEach( (button) => {
+    button.addEventListener('click', () => 
+      {console.log(button.innerText)})
+  })
 }

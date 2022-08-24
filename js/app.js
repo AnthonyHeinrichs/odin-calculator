450ee4a7eac0461050047554c58b97786e3c9112
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-08-24 12:37:11 +0200

    Added JS logic to concantenate strings when num or fun buttons are pressed. I also check to make sure two fun buttons are not being concatenated together

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 window.onload = function() {
 
+  let calcSteps = document.getElementById('calcSteps')
+  let calcResults = document.getElementById('calcSolution')
+
   const numButtons = document.getElementsByClassName('numButtons')
   const funButtons = document.getElementsByClassName('funButtons')
 
   Array.from(numButtons).forEach( (button) => {
     button.addEventListener('click', () => {
-      console.log(button.innerText)
+      let steps = calcSteps.innerText
+      if (isNaN(steps[steps.length - 1 ]) && 
+        !((steps[steps.length - 1 ]) === '.')) {
+          calcSteps.innerText = steps + ' ' + button.innerText
+      } else {
+        calcSteps.innerText = steps + button.innerText
+      }
     })
   })
 
   Array.from(funButtons).forEach ((button) => {
     button.addEventListener('click', () => {
-      {console.log(button.innerText)}
+      let steps = calcSteps.innerText
+      if (isNaN(steps[steps.length - 1 ])) {
+        return
+      } else {
+        buttonText = button.innerText
+        calcSteps.innerText = calcSteps.innerText + ' ' + buttonText + ' '
+      }
     })
   })
 }
